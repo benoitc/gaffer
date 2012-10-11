092498a2f78789ee2d09b2c5bbd05eeb7c4d8d52
benoitc <bchesneau@gmail.com>
2012-10-11 19:48:29 +0200

    refactor the code so we can reuse the code.

--- gaffer/http/stats.py
+++ gaffer/http/stats.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -
 #
-# This file is part of gaffer. See the NOTICE for more information.
+# this file is part of gaffer. see the notice for more information.
+
 import json
 
-from tornado.web import RequestHandler, asynchronous
+from tornado.web import asynchronous
 import pyuv
 
-class StatsHandler(RequestHandler):
+from .util import AsyncHandler
+
+class StatsHandler(AsyncHandler):
     """ watcher handler used to watch processes stats in quasi rt """
 
     def initialize(self, *args, **kwargs):
-        self._heartbeat = None
-        self._feed = None
-        self._closed = False
+        super(StatsHandler, self).__initialize__(self)
         self._source = None
 
     @asynchronous
@@ -73,58 +74,6 @@ class StatsHandler(RequestHandler):
             else:
                 return self.send_not_found()
 
-    def setup_stream(self, feed, m, heartbeat):
-        self._feed = feed
-
-        self.setup_heartbeat(heartbeat, m)
-        if feed == "eventsource":
-            self.set_header("Content-Type", "text/event-stream")
-        else:
-            self.set_header("Content-Type", "application/json")
-        self.set_header("Cache-Control", "no-cache")
-
-    def setup_heartbeat(self, heartbeat, m):
-        # set heartbeta
-        if heartbeat.lower() == "true":
-            heartbeat = 60000
-        else:
-            try:
-                heartbeat = int(heartbeat)
-            except TypeError:
-                heartbeat = False
-
-        if heartbeat:
-            self._heartbeat = pyuv.Timer(m.loop)
-            self._heartbeat.start(self._on_heartbeat, heartbeat,
-                    heartbeat)
-
-    def write_chunk(self, data):
-        chunk = "".join(("%X\r\n" % len(data), data, "\r\n"))
-        self.write(chunk)
-
-    def send_not_found(self):
-        self.set_status(404)
-        self.write({"error": "not_found"})
-        self.finish()
-
-    def _on_heartbeat(self, handle):
-        self.write("\n")
-
-    def _on_event(self, evtype, msg):
-        if self._feed == "eventsource":
-            event = ["event: %s" % evtype,
-                    "data: %s" % json.dumps(msg), ""]
-            self.write("\r\n".join(event))
-            self.flush()
-        else:
-            self.write("%s\r\n" % json.dumps(msg))
-            self.finish()
 
     def _handle_disconnect(self):
-        self._closed = True
         self._source.unmonitor(self._on_event)
-        if self._heartbeat is not None:
-            self._heartbeat.close()
-
-    def on_close_connection(self):
-        self._handle_disconnect()
---
+++ gaffer/http/util.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -
+#
+# this file is part of gaffer. see the notice for more information.
+
+from tornado.web import RequestHandler, asynchronous
+
+class AsyncHandler(RequestHandler):
+    """ watcher handler used to watch processes stats in quasi rt """
+
+    def initialize(self, *args, **kwargs):
+        self._heartbeat = None
+        self._feed = None
+        self._closed = False
+        self._source = None
+
+
+    def setup_stream(self, feed, m, heartbeat):
+        self._feed = feed
+
+        self.setup_heartbeat(heartbeat, m)
+        if feed == "eventsource":
+            self.set_header("Content-Type", "text/event-stream")
+        else:
+            self.set_header("Content-Type", "application/json")
+        self.set_header("Cache-Control", "no-cache")
+
+    def setup_heartbeat(self, heartbeat, m):
+        # set heartbeta
+        if heartbeat.lower() == "true":
+            heartbeat = 60000
+        else:
+            try:
+                heartbeat = int(heartbeat)
+            except TypeError:
+                heartbeat = False
+
+        if heartbeat:
+            self._heartbeat = pyuv.Timer(m.loop)
+            self._heartbeat.start(self._on_heartbeat, heartbeat,
+                    heartbeat)
+
+    def write_chunk(self, data):
+        chunk = "".join(("%X\r\n" % len(data), data, "\r\n"))
+        self.write(chunk)
+
+    def send_not_found(self):
+        self.set_status(404)
+        self.write({"error": "not_found"})
+        self.finish()
+
+    def _on_heartbeat(self, handle):
+        self.write("\n")
+
+    def _on_event(self, evtype, msg):
+        if self._feed == "eventsource":
+            event = ["event: %s" % evtype,
+                    "data: %s" % json.dumps(msg), ""]
+            self.write("\r\n".join(event))
+            self.flush()
+        else:
+            self.write("%s\r\n" % json.dumps(msg))
+            self.finish()
+
+    def handle_disconnect(self):
+        self._closed = True
+        self._handle_disconnect(self)
+        if self._heartbeat is not None:
+            self._heartbeat.close()
+
+    def on_close_connection(self):
+        self.handle_disconnect()
