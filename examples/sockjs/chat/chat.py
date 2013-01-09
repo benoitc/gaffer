105d871992a914267c6e968ca73d152177b829ce
benoitc <bchesneau@gmail.com>
2013-01-09 23:25:14 +0100

    add patched tornado_sockjs to gaffer
    This version is patched to use tornado_pyuv and add support to python 3.

---
+++ examples/sockjs/chat/chat.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+    Simple sockjs-tornado chat application. By default will listen on port 8080.
+"""
+
+from tornado import ioloop
+import tornado.web
+
+from gaffer.http import sockjs
+from gaffer import tornado_pyuv
+
+class IndexHandler(tornado.web.RequestHandler):
+    """Regular HTTP handler to serve the chatroom page"""
+    def get(self):
+        self.render('index.html')
+
+
+class ChatConnection(sockjs.SockJSConnection):
+    """Chat connection implementation"""
+    # Class level variable
+    participants = set()
+
+    def on_open(self, info):
+        # Send that someone joined
+        self.broadcast(self.participants, "Someone joined.")
+
+        # Add client to the clients list
+        self.participants.add(self)
+
+    def on_message(self, message):
+        # Broadcast message
+        self.broadcast(self.participants, message)
+
+    def on_close(self):
+        # Remove client from the clients list and broadcast leave message
+        self.participants.remove(self)
+
+        self.broadcast(self.participants, "Someone left.")
+
+if __name__ == "__main__":
+    import logging
+    logging.getLogger().setLevel(logging.DEBUG)
+
+    # 1. Create chat router
+    ChatRouter = sockjs.SockJSRouter(ChatConnection, '/chat')
+
+    # 2. Create Tornado application
+    app = tornado.web.Application(
+            [(r"/", IndexHandler)] + ChatRouter.urls
+    )
+
+    # 3. Make Tornado app listen on port 8080
+    app.listen(8080)
+
+    # 4. Start IOLoop
+    assert issubclass(ioloop.IOLoop, tornado_pyuv.IOLoop)
+    ioloop.IOLoop.instance().start()
