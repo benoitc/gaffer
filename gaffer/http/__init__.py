d7718960e7e31b3a5b4acb6c41badf7d2efa7d7c
benoitc <bchesneau@gmail.com>
2013-01-20 12:10:27 +0100

    move the pid resource to its own module.

--- gaffer/http/__init__.py
+++ gaffer/http/__init__.py
@@ -3,8 +3,8 @@
 # This file is part of gaffer. See the NOTICE for more information.
 
 from .misc import WelcomeHandler, StatusHandler, PingHandler, VersionHandler
-from .processes import (ProcessesHandler, ProcessIdHandler,
-        ProcessIdManageHandler, ProcessHandler, ProcessManagerHandler)
+from .process import ProcessesHandler, ProcessHandler, ProcessManagerHandler
+from .pid import ProcessIdHandler, ProcessIdManageHandler
 from .watcher import WatcherHandler
 from .stats import StatsHandler
 from .stream import StreamHandler, WStreamHandler
