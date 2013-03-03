93b2d867ce15b0ceebe654f5d85f1ab4217b7798
benoitc <bchesneau@gmail.com>
2013-03-03 12:21:32 +0100

    bump the version to 0.5

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ DATA_FILES = [
 
 
 setup(name='gaffer',
-      version="0.4.4",
+      version="0.5.0",
       description = 'simple system process manager',
       long_description = long_description,
       classifiers = CLASSIFIERS,
