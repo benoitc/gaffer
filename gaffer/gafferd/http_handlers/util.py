374e1df0ba6e8e84e19df3e088cad6a9c8d5b766
benoitc <bchesneau@gmail.com>
2013-03-13 15:26:53 +0100

    remove useless code

--- gaffer/gafferd/http_handlers/util.py
+++ gaffer/gafferd/http_handlers/util.py
@@ -55,7 +55,6 @@ class CorsHandler(RequestHandler):
             resp = {"error": status_code,
                     "reason": httplib.responses[status_code]}
 
-
         if self.settings.get("debug") and "exc_info" in kwargs:
             exc_info = traceback.format_exception(*kwargs["exc_info"])
             resp['exc_info'] = exc_info
@@ -84,74 +83,3 @@ class CorsHandlerWithAuth(CorsHandler):
 
     def _check_auth(self):
         return
-
-
-class AsyncHandler(CorsHandler):
-
-    def initialize(self, *args, **kwargs):
-        self._heartbeat = None
-        self._feed = None
-        self._closed = False
-        self._source = None
-        self._pattern = None
-
-    def setup_stream(self, feed, m, heartbeat):
-        self._feed = feed
-
-        self.setup_heartbeat(heartbeat, m)
-        if feed == "eventsource":
-            self.set_header("Content-Type", "text/event-stream")
-        else:
-            self.set_header("Content-Type", "application/json")
-        self.set_header("Cache-Control", "no-cache")
-
-    def setup_heartbeat(self, heartbeat, m):
-        # set heartbeta
-        if heartbeat.lower() == "true":
-            heartbeat = 60
-        else:
-            try:
-                heartbeat = int(heartbeat)
-            except TypeError:
-                heartbeat = False
-
-        if heartbeat:
-            self._heartbeat = pyuv.Timer(m.loop)
-            self._heartbeat.start(self._on_heartbeat, heartbeat,
-                    heartbeat)
-            self._heartbeat.unref()
-
-    def write_chunk(self, data):
-        chunk = "".join(("%X\r\n" % len(data), data, "\r\n"))
-        self.write(chunk)
-
-    def send_not_found(self):
-        self.set_status(404)
-        self.write({"error": "not_found"})
-        self.finish()
-
-    def _on_heartbeat(self, handle):
-        self.write("\n")
-
-    def _on_event(self, evtype, msg):
-        if self._feed == "eventsource":
-            event = ["event: %s" % evtype,
-                    "data: %s" % json.dumps(msg), ""]
-            self.write("\r\n".join(event))
-            self.flush()
-        else:
-            self.write("%s\r\n" % json.dumps(msg))
-            self.flush()
-            self.finish()
-
-    def handle_disconnect(self):
-        self._closed = True
-        self._handle_disconnect()
-        if self._heartbeat is not None:
-            self._heartbeat.close()
-
-    def on_finish(self):
-        self.handle_disconnect()
-
-    def on_close_connection(self):
-        self.handle_disconnect()
